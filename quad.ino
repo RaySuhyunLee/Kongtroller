fd9e6e4176fc151834cd3f491c422533d85d09eb
Suhyun Lee <raysuhyunlee@gmail.com>
2015-08-23 00:26:18 +0900

    p controller test. don't know exactly but some bugs exists...

--- quad.ino
+++ quad.ino
@@ -1,12 +1,13 @@
 #include <Servo.h>
 
-char wr;
-int isread = 1;
-
+const int MOTOR_START = 63;		// motor starts to turn when given this value.
+const int MOTOR_MAX = 70;			// for safety
 const int FRONT_LEFT_PIN = 5;
 const int FRONT_RIGHT_PIN = 4;
 const int BACK_LEFT_PIN = 2;
 const int BACK_RIGHT_PIN = 3;
+
+int isread = 1;
 
 Servo front_left;
 Servo front_right;
@@ -18,6 +19,7 @@ void init_motors() {
 	front_right.attach(FRONT_RIGHT_PIN);
 	back_left.attach(BACK_LEFT_PIN);
 	back_right.attach(BACK_RIGHT_PIN);
+	set_motors(0, 0, 0, 0);
 }
 
 // the setup routine runs once when you press reset:
@@ -30,17 +32,25 @@ void setup() {
   Serial3.begin(115200);
 }
 
-void set_motors(int fl, int fr, int bl, int br) {
-	front_left.write(fl);
-	front_right.write(fr);
-	back_left.write(bl);
-	back_right.write(br);
+int filter_bound(int value) {
+	if (value > MOTOR_MAX) {
+		value = MOTOR_MAX;
+	} else if (value < 0) {
+		value = 0;
+	}
+	return value;
 }
 
-void readMotion(int* roll, int* pitch, int* yaw) {
+void set_motors(int fl, int fr, int bl, int br) {
+	front_left.write(filter_bound(fl));
+	front_right.write(filter_bound(fr));
+	back_left.write(filter_bound(bl));
+	back_right.write(filter_bound(br));
+}
+
+void readMotion(short* roll, short* pitch, short* yaw) {
 	if (Serial3.available() > 0) {
 		char buffer[8];
-    short roll, pitch, yaw;
     
     while(1) {
       Serial3.readBytes(buffer, 2);
@@ -51,27 +61,31 @@ void readMotion(int* roll, int* pitch, int* yaw) {
     }
     
     Serial3.readBytes(buffer, 8);
-    roll = buffer[0] * 0x100 + buffer[1];
-    pitch = buffer[2] * 0x100 + buffer[3];
-    yaw = buffer[4] * 0x100 + buffer[5];
+    *roll = buffer[0] * 0x100 + buffer[1];
+    *pitch = buffer[2] * 0x100 + buffer[3];
+    *yaw = buffer[4] * 0x100 + buffer[5];
     
-    Serial.print(roll, DEC);
+    Serial.print(*roll, DEC);
     Serial.print(' ');
-    Serial.print(pitch, DEC);
+    Serial.print(*pitch, DEC);
     Serial.print(' ');
-    Serial.println(yaw, DEC);
+    Serial.println(*yaw, DEC);
   }
 }
 
-void pid_roll(double p, double i, double d) {
-	int out_front, out_back;
-	int roll, pitch, yaw;
+double pid_roll(double p, double i, double d, short roll, short desired) {
+	int out_left, out_right;
+	short in_diff;
+	double out_diff;
 
-	readMotion(&roll, &pitch, &yaw);
+	in_diff = desired - roll;
+	out_diff = in_diff * p;
+	return out_diff;
 }
 
 void sendCommand() {
 	/* give signal to motion sensor */
+	char wr;
   if (Serial.available() > 0) {
     wr = Serial.read();
     if (wr == '<') {
@@ -87,8 +101,29 @@ void sendCommand() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-	readMotion();
+	short roll, yaw, pitch;
+	double diff_roll;
+	int fl, fr, bl, br;
+	static int throttle = MOTOR_START+4;
+	readMotion(&roll, &yaw, &pitch);
+	diff_roll = pid_roll(1.0, 0, 0, roll, 0);
 
-	sendCommand();
-  delay(1);
+	diff_roll = map(diff_roll, -4000, 4000, -60, 60);
+	//Serial.println(diff_roll, DEC);
+
+	fl = throttle + diff_roll;
+	fr = throttle - diff_roll;
+	bl = throttle + diff_roll;
+	br = throttle - diff_roll;
+	set_motors(fl, fr, bl, br);
+	Serial.print(fl, DEC);
+	Serial.print(" ");
+	Serial.print(fr, DEC);
+	Serial.print(" ");
+	Serial.print(bl, DEC);
+	Serial.print(" ");
+	Serial.println(br, DEC);
+
+	//sendCommand();
+  delay(100);
 }
